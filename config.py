5835d5dd79990afb3c9dc94b0835c0da5ca83a2d
unknown <756541536@qq.com>
2019-04-11 21:19:25 +0800

    train params

--- config.py
+++ config.py
@@ -1,5 +1,72 @@
-epoches = 20
-lr = 1e-4
-l2_lambda = 1e-2
+# -----accuracy = 0.595625 ------
+# epoches = 20
+# lr = 1e-4
+# l2_lambda = 1e-2
+# batch_size = 256
+# momentum = 0.99
+# lr_decrease_ratio = 0.5
+# lr_decrease_epoch = 5
+# -------------------------------
+# -----accuracy = 0.590050 ------
+# epoches = 20
+# lr = 1e-3
+# l2_lambda = 1e-2
+# batch_size = 256
+# momentum = 0.99
+# lr_decrease_ratio = 0.85
+# lr_decrease_epoch = 10
+# -------------------------------
+# -----accuracy = 0.591810 ------
+# epoches = 20
+# lr = 5e-4
+# l2_lambda = 1e-2
+# batch_size = 256
+# momentum = 0.99
+# lr_decrease_ratio = 0.75
+# lr_decrease_epoch = 7
+# -------------------------------
+# -----accuracy = 0.603985 ------
+epoches = 40
+lr = 8e-5
+l2_lambda = 1e-3
 batch_size = 256
 momentum = 0.99
+lr_decrease_ratio = 0.95
+lr_decrease_epoch = 10
+# -------------------------------
+# -----accuracy = 0.603924 ------
+# epoches = 40
+# lr = 1e-4
+# l2_lambda = 1e-3
+# batch_size = 256
+# momentum = 0.99
+# lr_decrease_ratio = 0.95
+# lr_decrease_epoch = 15
+# -------------------------------
+# -----accuracy = 0.603050 ------
+# epoches = 40
+# lr = 1e-4
+# l2_lambda = 1e-3
+# batch_size = 256
+# momentum = 0.99
+# lr_decrease_ratio = 0.75
+# lr_decrease_epoch = 15
+# -------------------------------
+# -----accuracy = 0.603450 ------
+# epoches = 40
+# lr = 1e-4
+# l2_lambda = 1e-3
+# batch_size = 256
+# momentum = 0.99
+# lr_decrease_ratio = 0.1
+# lr_decrease_epoch = 15
+# -------------------------------
+# -----accuracy = 0.603670 ------
+# epoches = 40
+# lr = 1e-4
+# l2_lambda = 1e-3
+# batch_size = 256
+# momentum = 0.99
+# lr_decrease_ratio = 0.5
+# lr_decrease_epoch = 15
+# -------------------------------
